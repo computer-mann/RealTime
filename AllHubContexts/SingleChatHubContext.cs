5e87ec107bd95862e33b375e90e0b5036f160911
Prince Harry Nunoo <princeharry.nunoo@hubtel.com>
2024-10-22 11:39:09 +0000

    edged around the stone

--- AllHubContexts/SingleChatHubContext.cs
+++ AllHubContexts/SingleChatHubContext.cs
@@ -69,3 +69,21 @@ namespace realtime.AllHubContexts
         }
     }
 }
+
+
+
+/*
+ * 
+ * Add this block to nginx configuration 
+    location /notificationHub {
+    proxy_pass http://localhost:PORT_ON_WHICH_THE_SERVICE_RUNS/notificationHub;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;  
+    proxy_set_header Connection "upgrade"; 
+    proxy_buffering off;  
+    proxy_set_header Host $host;  
+    proxy_set_header X-Real-IP $remote_addr;  
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;  
+    proxy_set_header X-Forwarded-Proto $scheme;  
+    }
+ */
